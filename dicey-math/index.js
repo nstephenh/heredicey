b51dcf8375c414e3f0e0a23461dfd6c69ef249d1
Noah Stephen Haskell <nstephenh@gmail.com>
2024-01-09 09:38:13 -0600

    Get dicey to run locally, formatting changes, number of shots

--- dicey-math/index.js
+++ dicey-math/index.js
@@ -1,12 +1,12 @@
 let parser;
 
 try {
-  parser = require("../pegjs-loader.js!./parser.js");
+    parser = require("../pegjs-loader.js!./parser.js");
 } catch (e) {
-  parser = require("pegjs").generate(require("./parser.js"));
+    parser = require("pegjs").generate(require("./parser.js"));
 }
 
-let { debug, valueize, keyComp } = require("./utils");
+let {debug, valueize, keyComp} = require("./utils");
 
 let roll = require("./roll");
 
@@ -16,33 +16,81 @@ let roll = require("./roll");
  */
 
 class ParseResult {
-  constructor() {}
+    constructor() {
+    }
 
-  /**
-   * @returns {string}
-   */
-  get debugString() {
-    return debug(this.parsed);
-  }
+    /**
+     * @returns {string}
+     */
+    get debugString() {
+        return debug(this.parsed);
+    }
 
-  /**
-   * @returns {Block}
-   */
-  get result() {
-    if (!this.value) this.value = valueize(this.parsed);
-    return this.value;
-  }
+    /**
+     * @returns {Block}
+     */
+    get result() {
+        const bal_skill = 4; //4+ to hit
+        const num_shots = 4;
+        const WEAPON_AP = 6;
+        const UNIT_SAVE = 3; //X + save
+        let unit_save = UNIT_SAVE
 
-  roll() {
-    return roll(this.parsed);
-  }
+        if (WEAPON_AP <= unit_save) {
+            unit_save = 7
+        }
+        const successful_hit_expressions = {
+            "type": "math",
+            "right": bal_skill,
+            "left": {
+                "type": "die",
+                "times": num_shots,
+                "sides": 6
+            },
+            "m": "cs",
+            "op": ">="
+        }
 
-  /**
-   * @returns {Output}
-   */
-  output() {
-    return this.result.output();
-  }
+        function multiply_clouds(a, b) {
+            return {
+                "type": "call",
+                "name": "repeat",
+                "args": [a, b]
+            }
+        }
+
+        const saves_expression = {
+            "type": "math",
+            "left": multiply_clouds({
+                "type": "die",
+                "sides": 6
+            }, successful_hit_expressions),
+            "right": unit_save,
+            "m": "cs",
+            "op": "<"
+        }
+        this.parsed = {body: []}
+        this.parsed.type = "block";
+        this.parsed.body[0] = {
+            "type": "output",
+            "expression": successful_hit_expressions,
+            "text": `Successful hits with ${num_shots} shots at BS ${bal_skill}`,//`Fail a ${unit_save}+ Save vs an ${num_shots} shots at ${bal_skill}, STR TBD, AP ${WEAPON_AP} weapon`,
+            "name": null
+        }
+        if (!this.value) this.value = valueize(this.parsed);
+        return this.value;
+    }
+
+    roll() {
+        return roll(this.parsed);
+    }
+
+    /**
+     * @returns {Output}
+     */
+    output() {
+        return this.result.output();
+    }
 }
 
 /**
@@ -50,20 +98,32 @@ class ParseResult {
  * @param {String} code
  * @returns {ParseResult}
  */
-module.exports = (code) => {
-  let o = new ParseResult();
-  o.parsed = parser.parse(code);
-  return o;
+module
+    .exports = (code) => {
+    let o = new ParseResult();
+    o.parsed = parser.parse(code);
+    return o;
 };
 
-Object.assign(module.exports, {
-  parser,
-  rehydrate: (i) => {
-    let o = new ParseResult();
-    o.parsed = i.parsed;
-    return o;
-  },
-  debug,
-  valueize,
-  keyComp,
-});
+Object
+    .assign(module
+
+            .exports
+        , {
+            parser
+            ,
+            rehydrate: (i) => {
+                let o = new ParseResult();
+                o.parsed = i.parsed;
+                return o;
+            }
+            ,
+            debug
+            ,
+            valueize
+            ,
+            keyComp
+            ,
+        }
+    )
+;
--- dicey-site/src/state.js
+++ dicey-site/src/state.js
@@ -1,79 +1,79 @@
-import { atom, selector } from "recoil";
+import {atom, selector} from "recoil";
 
 import compile from "dicey.js";
-import worker from "!!workerize-loader!./worker"; // eslint-disable-line import/no-webpack-loader-syntax
+
+const worker = () => require('./worker.js') // https://github.com/developit/workerize-loader/issues/131
 
 let workerInstance = worker();
 
-export let reload = atom({ key: "reload", default: Math.random() });
-export let query = atom({ key: "query", default: "output 3d6" });
+export let reload = atom({key: "reload", default: Math.random()});
+export let query = atom({key: "query", default: "output 3d6"});
 export let parsed = selector({
-  key: "parsed",
-  get: ({ get }) => {
-    let expression = get(query);
-    try {
-      let x = compile(expression);
-      return { ok: true, v: x };
-    } catch (e) {
-      return { ok: false, error: e };
-    }
-  },
+    key: "parsed",
+    get: ({get}) => {
+        let expression = get(query);
+        try {
+            let x = compile(expression);
+            return {ok: true, v: x};
+        } catch (e) {
+            return {ok: false, error: e};
+        }
+    },
 });
 
-export let showReload = atom({ key: "showReload", default: false });
-export let waitingWorker = atom({ key: "waitingWorker", default: null });
+export let showReload = atom({key: "showReload", default: false});
+export let waitingWorker = atom({key: "waitingWorker", default: null});
 
-export let collapse = atom({ key: "collapse", default: true });
-export let mode = atom({ key: "mode", default: "normal" });
-export let transpose = atom({ key: "transpose", default: false });
+export let collapse = atom({key: "collapse", default: true});
+export let mode = atom({key: "mode", default: "normal"});
+export let transpose = atom({key: "transpose", default: false});
 
 let working = false;
 
-export let sample = atom({ key: "sample", default: [] });
-export let fresh = atom({ key: "fresh", default: false });
-export let sampleError = atom({ key: " sampleError", default: null });
+export let sample = atom({key: "sample", default: []});
+export let fresh = atom({key: "fresh", default: false});
+export let sampleError = atom({key: " sampleError", default: null});
 export let activeSample = selector({
-  key: "selector",
-  get: async ({ get }) => {
-    let p = get(parsed);
-    let c = get(collapse);
-    let m = get(mode);
-    let t = get(transpose);
-    if (!p.ok) return false;
+    key: "selector",
+    get: async ({get}) => {
+        let p = get(parsed);
+        let c = get(collapse);
+        let m = get(mode);
+        let t = get(transpose);
+        if (!p.ok) return false;
 
-    if (working) {
-      workerInstance.terminate();
-      workerInstance = worker();
-    } else {
-      working = true;
-    }
+        if (working) {
+            workerInstance = worker();
+        } else {
+            working = true;
+        }
 
-    let result = await workerInstance.compute({
-      parsed: p,
-      collapse: c,
-      mode: m,
-      transpose: t,
-    });
-    working = false;
-    return result;
-  },
+        let result = await workerInstance.compute({
+            parsed: p,
+            collapse: c,
+            mode: m,
+            transpose: t,
+        });
+        working = false;
+        return result;
+    },
 });
 
 export let errors = selector({
-  key: "error",
-  get: ({ get }) => {
-    let p = get(parsed);
+    key: "error",
+    get: ({get}) => {
+        let p = get(parsed);
 
-    if (!p.ok) {
-      let s = `${p.error.name}: `;
-      if (p.error.location)
-        s += ` ${p.error.location.start.line}:${p.error.location.start.column}`;
-      s += ` ${p.error.message}`;
-      return s;
-    }
+        if (!p.ok) {
+            let s = `${p.error.name}: `;
+            if (p.error.location)
+                s += ` ${p.error.location.start.line}:${p.error.location.start.column}`;
+            s += ` ${p.error.message}`;
+            return s;
+        }
 
-    let se = get(sampleError);
-    if (se) return se;
-    return false;
-  },
+        let se = get(sampleError);
+        if (se) return se;
+        return false;
+    },
 });
