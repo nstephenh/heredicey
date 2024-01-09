c8af01c521f13798cc20fbcb8ef4e860103f0683
Noah Stephen Haskell <nstephenh@gmail.com>
2024-01-09 10:14:20 -0600

    Print hits, wounds, and failed saves.

--- dicey-math/index.js
+++ dicey-math/index.js
@@ -30,22 +30,25 @@ class ParseResult {
      * @returns {Block}
      */
     get result() {
-        const bal_skill = 4; //4+ to hit
-        const num_shots = 4;
+        const bal_skill = 3; //X+ to hit
+        const to_hit = 7-bal_skill;
+
+
+        const num_shots = 2;
         const weapon_strength = 4;
         const target_toughness = 4;
         const WEAPON_AP = 6;
-        const UNIT_SAVE = 3; //X + save
-        let unit_save = UNIT_SAVE
 
-        let toughness_target = Math.max(weapon_strength-target_toughness + 4, 2)
+        let target_save = 3; //X+ save
 
-        if (WEAPON_AP <= unit_save) {
-            unit_save = 7
+        let to_wound_roll = Math.max(weapon_strength - target_toughness + 4, 2)
+
+        if (WEAPON_AP <= target_save) {
+            target_save = 7
         }
-        const successful_hit_expressions = {
+        const successful_hits = {
             "type": "math",
-            "right": bal_skill,
+            "right": to_hit,
             "left": {
                 "type": "die",
                 "times": num_shots,
@@ -55,21 +58,24 @@ class ParseResult {
             "op": ">="
         }
 
-        function multiply_clouds(a, b) {
-            return {
-                "type": "call",
-                "name": "repeat",
-                "args": [a, b]
-            }
+        const successful_wounds = {
+            "type": "math",
+            "right": to_wound_roll,
+            "left": multiply_clouds({
+                "type": "die",
+                "sides": 6
+            }, successful_hits),
+            "m": "cs",
+            "op": ">="
         }
 
-        const saves_expression = {
+        const failed_saves = {
             "type": "math",
             "left": multiply_clouds({
                 "type": "die",
                 "sides": 6
-            }, successful_hit_expressions),
-            "right": unit_save,
+            }, successful_wounds),
+            "right": target_save,
             "m": "cs",
             "op": "<"
         }
@@ -77,8 +83,20 @@ class ParseResult {
         this.parsed.type = "block";
         this.parsed.body[0] = {
             "type": "output",
-            "expression": saves_expression,
-            "text": `Fail a ${unit_save}+ Save vs an ${num_shots} shots at ${bal_skill}, STR TBD, AP ${WEAPON_AP} weapon`,
+            "expression": successful_hits,
+            "text": `Hits from ${num_shots} shots at BS ${bal_skill}`,
+            "name": null
+        }
+        this.parsed.body[1] = {
+            "type": "output",
+            "expression": successful_wounds,
+            "text": `Wounds from ${num_shots} shots at BS ${bal_skill}, STR ${weapon_strength} weapon on a T ${target_toughness} target`,
+            "name": null
+        }
+        this.parsed.body[2] = {
+            "type": "output",
+            "expression": failed_saves,
+            "text": `Wounds from ${num_shots} shots at BS ${bal_skill}, STR ${weapon_strength}, AP ${WEAPON_AP} weapon on a T ${target_toughness}, ${target_save}+ target`,
             "name": null
         }
         if (!this.value) this.value = valueize(this.parsed);
@@ -94,6 +112,14 @@ class ParseResult {
      */
     output() {
         return this.result.output();
+    }
+}
+
+function multiply_clouds(a, b) {
+    return {
+        "type": "call",
+        "name": "repeat",
+        "args": [a, b]
     }
 }
 
